22afdbd45a13e7f2993dc76dc3711e96c34320a3
BePine <bepine17@gmail.com>
2022-08-26 17:04:47 +0200

    refined some css in main and buttons

--- src/Pages/Home/Main.tsx
+++ src/Pages/Home/Main.tsx
@@ -1,8 +1,11 @@
 const Main = () => {
-    const lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam euismod justo magna, vitae luctus lorem finibus at. Cras aliquet malesuada augue. Vestibulum id aliquet justo. Donec sit amet enim purus. Integer velit ex, efficitur a tempus ut, mollis suscipit nunc. Vivamus auctor ipsum ac mauris suscipit, eget tempus nunc malesuada. Integer placerat lacinia magna sed finibus. Proin vestibulum lacus imperdiet elit aliquam commodo. Nunc elit purus, bibendum quis est a, egestas convallis metus. Donec vehicula justo eu aliquet hendrerit. Praesent orci nisl, mattis gravida facilisis id, tincidunt vel lectus. Vestibulum aliquet maximus nulla, sed laoreet quam tincidunt nec. Aliquam luctus molestie tempus. Mauris tristique consectetur finibus. Nullam nisl sem, commodo nec suscipit nec, bibendum vitae ligula."
+    // const lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam euismod justo magna, vitae luctus lorem finibus at. Cras aliquet malesuada augue. Vestibulum id aliquet justo. Donec sit amet enim purus. Integer velit ex, efficitur a tempus ut, mollis suscipit nunc. Vivamus auctor ipsum ac mauris suscipit, eget tempus nunc malesuada. Integer placerat lacinia magna sed finibus. Proin vestibulum lacus imperdiet elit aliquam commodo. Nunc elit purus, bibendum quis est a, egestas convallis metus. Donec vehicula justo eu aliquet hendrerit. Praesent orci nisl, mattis gravida facilisis id, tincidunt vel lectus. Vestibulum aliquet maximus nulla, sed laoreet quam tincidunt nec. Aliquam luctus molestie tempus. Mauris tristique consectetur finibus. Nullam nisl sem, commodo nec suscipit nec, bibendum vitae ligula."
     return(
         <>
-            <div className="content">{lorem}</div>
+            <h2>About us</h2>
+            <div className="content">Workflow is our application dedicated for people looking forward to organize
+            their workflow as the name suggests. With workflow you can easily schedule all your incoming meetings, 
+            manage your todo list and plan upcoming days of work.</div>
         </>
     )
 }
